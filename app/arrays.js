d1a615109cf0e87cba846da26ba1844b6f397f92
Alexander Miranda <alex.miranda@noaa.gov>
2016-02-12 09:43:09 -0800

    New solutions to js-assessment

--- app/arrays.js
+++ app/arrays.js
@@ -11,31 +11,45 @@ exports.arraysAnswers = {
   },
 
   remove : function(arr, item) {
-
+    arr.forEach(function(el, i, arr) {
+      if (el === item) {
+        arr.splice(i, 1);
+      }
+    });
+    return arr;
   },
 
   removeWithoutCopy : function(arr, item) {
-
+    arr.map(function(el, i, arr) {
+      if (el === item) {
+        arr.splice(i, 1);
+      }
+    });
+    return arr;
   },
 
   append : function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate : function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend : function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail : function(arr) {
-
+    arr.splice(0, 1);
+    return arr;
   },
 
   concat : function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert : function(arr, item, index) {
@@ -43,7 +57,13 @@ exports.arraysAnswers = {
   },
 
   count : function(arr, item) {
-
+    var count = 0;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        count++;
+      }
+    }
+    return count;
   },
 
   duplicates : function(arr) {
@@ -51,7 +71,9 @@ exports.arraysAnswers = {
   },
 
   square : function(arr) {
-
+    return arr.map(function(el) {
+      return el * el;
+    });
   },
 
   findAllOccurrences : function(arr, target) {
